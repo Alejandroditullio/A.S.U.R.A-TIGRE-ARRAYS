523ab8585f7a000d078095ac0607993748f68638
Alejandroditullio <105236589+Alejandroditullio@users.noreply.github.com>
2022-08-28 23:31:35 -0300

    entrega final

--- js/scripts.js
+++ js/scripts.js
@@ -1,212 +1,3 @@
-/*let x = 10;
-let y = 15;
-let cartel1= "hola";
-let cartel2= "mundo";
-
-console.log (x + y);
-console.log (cartel1 + " " + cartel2);*/
-
-
-
-/*let nombre = prompt("ingrese su nombre por favor.");
-let edad = prompt("ingrese su edad");
-
-for(let i=1; i <=10; + i ++);{
-
-    alert("bienvenido " + nombre + " tu tienes " + edad );
-}*/
-
-
-
-
-/*let num = prompt("ingrese un numero")
-if(num <= 10 && num >= 50){
-    console.log("el numero es $ {num} esta entre 10 y 50 ");
-
-}else{
-    console.log("el numero es $ {num} no esta entre 10 y 50 ");
-}*/
-
-
-
-/*let tabla =9;
-let resultado;
-
-for (let i= 1; i <=10; i + i ++ ){
-    resultado=tabla * i;
-    console.log (tabla + "x" + i + "es igual a" + resultado);
-}*/
-
-
-
-//intento de buscador de reciclaje asura por localidad //
-
-/*let diasdereciclaje = prompt ("punto reciclaje");*/
-
-/*alert("ud puede reciclar en " + "ingrese localidad" + " localidad " );*/
-
-/*for ( let i=1 ; i <=7; i++ );*/
-
-
-
-  
-
-
-
- 
-
-//lo de arriba es practica // 
-/*
-   
-     let opcionUser = prompt(`
-    Elija una opción:
-    1: Libros
-    2: Películas
-    3: Juegos
-    4:ESC para finalizar
-    `);
-    
- 
- while(opcionUser != "ESC" ){
-    switch (opcionUser) {
-    case "1":
-    alert("Principito"); }
-           
-    switch (opcionUser) {
-    case "2":
-    alert("MATRIX"); }
-    switch (opcionUser) {
-     case "3":
-    alert("Mortal Kombat"); }
-
-    
-
-      opcionUser = prompt(`
-      Elija una opción:
-      1: Libros
-      2: Películas
-      3: Juegos
-      4:ESC para finalizar
-      `);
-  }*/
-
-
-/////// proyecto de reciclaje entrega 2//////
-
-/////las array estan en el principio///
-
-/*
-  const hoja=[ 25 ];
-  const metal= [10];
-  const cajas= [80];
-  const pet = [0.75];
-  
-  const  papel = (pesos) => pesos * hoja;
-  const  aluminio = (pesos) => pesos * metal;
-  const  carton= (pesos) => pesos * cajas;
-  const  plastico= (pesos) => pesos * pet;
- 
-  let seleccion = prompt("que desea reciclar \n 1 - papel \n 2 -  aluminio \n 3 -  carton \n 4 -  plastico ");
-  let valor = prompt("cantidad de kg");
-
-  switch (seleccion) {
-      case "1":
-          alert(papel(valor));
-          break;
-      case "2":
-          alert(aluminio(valor));
-          break;
-     case"3":
-          alert(carton(valor));
-          break;
-     case"4":
-        alert(plastico(valor))
-      
-
-}
-alert("gracias por usar nuestra web");
-
-console.log(seleccion , valor);
-
-*/
-/*
-const resultado = document.getElementById("resultado");
-const container = document.querySelector(".container");
-let contador = 0;
-
-container.addEventListener("click", (e) => {
- 
-if (e.target.classList.contains("btn-danger")) {
-    contador--;
-    pintarContador();
-  }
-
-if (e.target.classList.contains("btn-info")) {
-    contador++;
-    pintarContador();
-    
-  }
-  e.stopPropagation()
-});
-
-document.body.addEventListener('click', e => {console.log('body')})
-
-pintarContador = () => {
-  resultado.textContent = contador;
-};
-
-document.getElementById("container py-5 text-center")= ("")*/
-
-
-
-
-/*let carroproductos = [];
-
-function meteralcarrito(carroproductos){
-  console.log(carroproductos)
-  /*carroproductos.push(productos);
-  pintandocarroproductos();*/
-
-
-
-/*function pintandolista(){
-  let aux = '';
-  for(let i = 0; i < productos.length; i++){
-    aux = aux +
-     `<div oneclick="meteralcarrito({id:${productos[i].id}, nombre:${productos[i].nombre},precio:${productos[i].precio} }) " style="cursor: pointer;border: 1px solid black; margin: 8px; width:200px;
-      " >
-          <h5>${productos[i].nombre} </h5>
-          <p>${productos[i].precio} </p>
-          <p>id:${productos[i].id} </p>
-     </div>`;
-    
-    
-  }
-    document.getElementById(`productos`).innerHTML=aux;
-}
-pintandolista();
-
-
-
-function pintandocarroproductos(){
-  let aux = '';
-  for(let i = 0; i < carroproductos.length; i++){
-    aux = aux +
-     `<div oneclick="meteralcarrito({id:${carroproductos[i].id},nombre:${carroproductos[i].nombre},precio:${carroproductos[i].precio} }) 
-      ">
-          <h5>${carroproductos[i].nombre} </h5>
-          <p>${carroproductos[i].precio} </p>
-          <p>id:${carroproductos[i].id} </p>
-     </div>`;
-    
-    
-  }
-    document.getElementById(`carro`).innerHTML=aux;
-}
-*/
-
-
-
 const Clickbutton = document.querySelectorAll('.button')
 const tbody = document.querySelector('.tbody')
 let carrito = []
@@ -214,7 +5,6 @@ let carrito = []
 Clickbutton.forEach(btn => {
   btn.addEventListener('click', addToCarritoItem)
 })
-
 
 function addToCarritoItem(e){
   const button = e.target
@@ -261,7 +51,7 @@ function addItemCarrito(newItem){
 
 
 function renderCarrito(){
-  tbody.innerHTML = ''
+  
   carrito.map(item => {
     const tr = document.createElement('tr')
     tr.classList.add('ItemCarrito')
@@ -296,7 +86,7 @@ function CarritoTotal(){
     Total = Total + precio*item.cantidad
   })
 
-  itemCartTotal.innerHTML = `Total $${Total}`
+  itemCartTotal.innerHTML = `Total $$ {Total}`
   addLocalStorage()
 }
 
@@ -349,73 +139,35 @@ window.onload = function(){
 
 
 
-//registro de nuevos usuarios//
-
-var formulario = document.getElementById('formulario');
-var respuesta = document.getElementById('respuesta');
-
-formulario.addEventListener('submit', function(e){
-    e.preventDefault();
-    console.log('')
-
-    var datos = new FormData(formulario);
-
-    console.log(datos)
-    console.log(datos.get('usuario'))
-    console.log(datos.get('pass'))
-
-    fetch('post.php',{
-        method: 'POST',
-        body: datos
-    })
-        .then( res => res.json())
-        .then( data => {
-            console.log(data)
-            if(data === 'error'){
-                respuesta.innerHTML = `
-                <div class="alert alert-danger" role="alert">
-                    Llena todos los campos
-                </div>
-                `
-            }else{
-                respuesta.innerHTML = `
-                <div class="alert alert-primary" role="alert">
-                    ${data}
-                </div>
-                `
-            }
-        } )
-})
 
 
 
-///datos//
 
-var contenido = document.querySelector('#contenido' )
 
-  function traer() {
-    fetch('data.json')
-      .then(res => res.json())
-      .then(datos => {
-      // console.log(datos)
-       tabla(datos)
-            })
-        }
 
-  function tabla(datos) {
-            // console.log(datos)
-   contenido.innerHTML = ''
-      for(let valor of datos){
-      // console.log(valor.nombre)
-       contenido.innerHTML += `
-                
-        <tr>
-         <th scope="row">${ valor.id }</th>
-         <td>${ valor.nombre }</td>
-         <td>${ valor.email }</td>
-          <td>${ valor.estado ? "Activo" : "Eliminado" }</td>
-           </tr>
-                
-            `
-      }
-   }
+////////////////////
+
+function capturar(){
+  
+  function Persona(nombre,contraseña){
+    this.usuario=nombre;
+    this.contraseña=contraseña;
+    
+  }
+  var nombrecapturar= document.getElementById("usuario").value;
+  
+  var contraseñacapturar= document.getElementById("contraseña").value;
+
+  nuevosujeto = new Persona(nombrecapturar,contraseñacapturar);
+  console.log(nuevosujeto);
+  agregar();
+}
+
+var basedatos= [];
+function agregar(){
+  basedatos.push(nuevosujeto);
+  console.log(basedatos);
+  document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevosujeto.usuario+ '</td><td>'+nuevosujeto.contraseña+ '</td></tbody>';
+};
+
+ 
